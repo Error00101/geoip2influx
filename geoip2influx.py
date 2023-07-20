be415563024c6048d4bbf7ba343ade2adf239554
Error00101 <54121491+Error00101@users.noreply.github.com>
2023-07-20 22:22:36 +0200

    Update geoip2influx.py
    change from influx to questdb

--- geoip2influx.py
+++ geoip2influx.py
@@ -11,7 +11,7 @@ import logging
 
 from geoip2.database import Reader
 from geohash2 import encode
-from influxdb import InfluxDBClient
+from questdb.ingress import Sender
 from requests.exceptions import ConnectionError
 from influxdb.exceptions import InfluxDBServerError, InfluxDBClientError
 from IPy import IP as ipadd
@@ -23,10 +23,6 @@ log_path = env.get('NGINX_LOG_PATH', '/config/log/nginx/access.log')
 influxdb_host = env.get('INFLUX_HOST', 'localhost')
 influxdb_port = env.get('INFLUX_HOST_PORT', '8086')
 influxdb_database = env.get('INFLUX_DATABASE', 'geoip2influx')
-influxdb_user = env.get('INFLUX_USER', 'root')
-influxdb_user_pass = env.get('INFLUX_PASS', 'root')
-influxdb_retention = env.get('INFLUX_RETENTION','7d')
-influxdb_shard = env.get('INFLUX_SHARD', '1d')
 geo_measurement = env.get('GEO_MEASUREMENT', 'geoip2influx')
 log_measurement = env.get('LOG_MEASUREMENT', 'nginx_access_logs')
 send_nginx_logs = env.get('SEND_NGINX_LOGS','true')
@@ -105,8 +101,8 @@ def file_exists(log_path,geoip_db_path):
 
 
 def logparse(
-        log_path, influxdb_host, influxdb_port, influxdb_database, influxdb_user, influxdb_user_pass, influxdb_retention,
-        influxdb_shard, geo_measurement, log_measurement, send_nginx_logs, geoip_db_path, inode):
+        log_path, influxdb_host, influxdb_port, influxdb_database,  
+        geo_measurement, log_measurement, send_nginx_logs, geoip_db_path, inode):
     # Preparing variables and params
     ips = {}
     geohash_fields = {}
@@ -115,39 +111,6 @@ def logparse(
     log_data_tags = {}
     nginx_log = {}
     hostname = uname()[1]
-    client = InfluxDBClient(
-        host=influxdb_host, port=influxdb_port, username=influxdb_user, password=influxdb_user_pass, database=influxdb_database)
-
-    try:
-        logging.debug('Testing InfluxDB connection')
-        version = client.request('ping', expected_response_code=204).headers['X-Influxdb-Version']
-        logging.debug(f'Influxdb version: {version}')
-    except ConnectionError as e:
-        logging.critical('Error testing connection to InfluxDB. Please check your url/hostname.\n'
-                         f'Error: {e}'
-                        )
-        exit(1)
-
-    try:
-        databases = [db['name'] for db in client.get_list_database()]
-        if influxdb_database in databases:    
-            logging.debug(f'Found database: {influxdb_database}')
-    except InfluxDBClientError as e:
-        logging.critical('Error getting database list! Please check your InfluxDB configuration.\n'
-                         f'Error: {e}'
-                        )
-        exit(1)
-
-    if influxdb_database not in databases:
-        logging.info(f'Creating database: {influxdb_database}')
-        client.create_database(influxdb_database)
-
-        retention_policies = [policy['name'] for policy in client.get_list_retention_policies(database=influxdb_database)]
-        if f'{influxdb_database} {influxdb_retention}-{influxdb_shard}' not in retention_policies:
-            logging.info(f'Creating {influxdb_database} retention policy ({influxdb_retention}-{influxdb_shard})')
-            client.create_retention_policy(name=f'{influxdb_database} {influxdb_retention}-{influxdb_shard}', duration=influxdb_retention, replication='1',
-                                                database=influxdb_database, default=True, shard_duration=influxdb_shard)
-
     re_ipv4 = compile(r'(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (?P<remote_user>.+) \[(?P<dateandtime>\d{2}\/[A-Z]{1}[a-z]{2}\/\d{4}:\d{2}:\d{2}:\d{2} ((\+|\-)\d{4}))\](["](?P<method>.+)) (?P<referrer>.+) ((?P<http_version>HTTP\/[1-3]\.[0-9])["]) (?P<status_code>\d{3}) (?P<bytes_sent>\d{1,99})(["](?P<url>(\-)|(.+))["]) (?P<host>.+) (["](?P<user_agent>.+)["])(["](?P<request_time>.+)["]) (["](?P<connect_time>.+)["])(["](?P<city>.+)["]) (["](?P<country_code>.+)["])', IGNORECASE) # NOQA
     re_ipv6 = compile(r'(?P<ipaddress>(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))) - (?P<remote_user>.+) \[(?P<dateandtime>\d{2}\/[A-Z]{1}[a-z]{2}\/\d{4}:\d{2}:\d{2}:\d{2} ((\+|\-)\d{4}))\](["](?P<method>.+)) (?P<referrer>.+) ((?P<http_version>HTTP\/[1-3]\.[0-9])["]) (?P<status_code>\d{3}) (?P<bytes_sent>\d{1,99})(["](?P<url>(\-)|(.+))["]) (?P<host>.+) (["](?P<user_agent>.+)["])(["](?P<request_time>.+)["]) (["](?P<connect_time>.+)["])(["](?P<city>.+)["]) (["](?P<country_code>.+)["])', IGNORECASE) # NOQA
 
@@ -196,7 +159,7 @@ def logparse(
                     log = re_ipv6
                 else:
                     logging.warning('Failed to match regex that previously matched!? Skipping this line!\n'
-                                    'If you think the regex should have mathed the line, please share the log line below on https://discord.gg/HSPa4cz or Github: https://github.com/gilbN/geoip2influx\n' 
+                                    'If you think the regex should have mathed the line, please share the log line below on https://discord.gg/HSPa4cz or Github: https://github.com/gilbN/geoip2influx\n'
                                     f'Line: {line}'
                                    )
                     continue
@@ -205,9 +168,6 @@ def logparse(
                     info = gi.city(ip)
                     if info:
                         geohash = encode(info.location.latitude, info.location.longitude)
-                        geohash_fields['count'] = 1
-                        geohash_tags['geohash'] = geohash
-                        geohash_tags['ip'] = ip
                         geohash_tags['host'] = hostname
                         geohash_tags['country_code'] = info.country.iso_code
                         geohash_tags['country_name'] = info.country.name
@@ -215,19 +175,20 @@ def logparse(
                         geohash_tags['state_code'] = info.subdivisions.most_specific.iso_code if info.subdivisions.most_specific.iso_code else "-"
                         geohash_tags['city'] = info.city.name if info.city.name else "-"
                         geohash_tags['postal_code'] = info.postal.code if info.postal.code else "-"
-                        geohash_tags['latitude'] = info.location.latitude if info.location.latitude else "-"
-                        geohash_tags['longitude'] = info.location.longitude if info.location.longitude else "-"
+                        geohash_tags['measurement'] = geo_measurement
+                        geohash_fields['latitude'] = info.location.latitude if info.location.latitude else "-"
+                        geohash_fields['longitude'] = info.location.longitude if info.location.longitude else "-"
+                        geohash_fields['ip'] = ip
+                        geohash_fields['geohash'] = geohash
+                        geohash_fields['count'] = 1
                         ips['tags'] = geohash_tags
                         ips['fields'] = geohash_fields
                         ips['measurement'] = geo_measurement
                         geo_metrics.append(ips)
                         logging.debug(f'Geo metrics: {geo_metrics}')
-                        try:
-                            client.write_points(geo_metrics)
-                        except (InfluxDBServerError, ConnectionError) as e:
-                            logging.error('Error writing data to InfluxDB! Check your database!\n'
-                                          f'Error: {e}'
-                                         )
+                        with Sender(influxdb_host, influxdb_port) as sender:
+                            sender.row(influxdb_database+'_geo', symbols=geohash_tags, columns=geohash_fields)
+                            sender.flush()
                 else:
                     logging.debug(f"Incorrect IP type: {ip_type}")
                 if send_logs:
@@ -243,34 +204,31 @@ def logparse(
                                 log_data_fields['connect_time'] = float(datadict['connect_time']) if datadict['connect_time'] != '-' else 0.0
                             except ValueError:
                                 log_data_fields['connect_time'] = str(datadict['connect_time'])
-                            log_data_tags['ip'] = datadict['ipaddress']
-                            log_data_tags['datetime'] = datetime.strptime(datadict['dateandtime'], '%d/%b/%Y:%H:%M:%S %z')
-                            log_data_tags['remote_user'] = datadict['remote_user']
-                            log_data_tags['method'] = datadict['method']
-                            log_data_tags['referrer'] = datadict['referrer']
+                            log_data_fields['ip'] = datadict['ipaddress']
+                            log_data_fields['datetime'] = datetime.strptime(datadict['dateandtime'], '%d/%b/%Y:%H:%M:%S %z')
+                            log_data_fields['remote_user'] = datadict['remote_user']
+                            log_data_fields['referrer'] = datadict['referrer']
+                            log_data_fields['bytes_sent'] = datadict['bytes_sent']
+                            log_data_fields['url'] = datadict['url']
+                            log_data_fields['request_time'] = datadict['request_time']
+                            log_data_fields['connect_time'] = datadict['connect_time']
+                            log_data_fields['user_agent'] = datadict['user_agent']
                             log_data_tags['host'] = datadict['host']
-                            log_data_tags['http_version'] = datadict['http_version']
+                            log_data_tags['method'] = datadict['method']
                             log_data_tags['status_code'] = datadict['status_code']
-                            log_data_tags['bytes_sent'] = datadict['bytes_sent']
-                            log_data_tags['url'] = datadict['url']
-                            log_data_tags['user_agent'] = datadict['user_agent']
-                            log_data_tags['request_time'] = datadict['request_time']
-                            log_data_tags['connect_time'] = datadict['connect_time']
+                            log_data_tags['http_version'] = datadict['http_version']
                             log_data_tags['city'] = datadict['city']
                             log_data_tags['country_code'] = datadict['country_code']
                             log_data_tags['country_name'] = info.country.name
+                            log_data_tags['measurement'] = log_measurement
                             nginx_log['tags'] = log_data_tags
                             nginx_log['fields'] = log_data_fields
                             nginx_log['measurement'] = log_measurement
                             log_metrics.append(nginx_log)
                             logging.debug(f'NGINX log metrics: {log_metrics}')
-                            try:
-                                client.write_points(log_metrics)
-                            except (InfluxDBServerError, InfluxDBClientError, ConnectionError) as e:
-                                logging.error('Error writing data to InfluxDB! Check your database!\n'
-                                            f'Error: {e}'
-                                            )
-
+                            with Sender(influxdb_host, influxdb_port) as sender:
+                                sender.row(influxdb_database+'_log', symbols=log_data_tags, columns=log_data_fields)
+                                sender.flush()
 
 def main():
     logging.info('Starting geoip2influx..')
@@ -281,10 +239,6 @@ def main():
     f'\n -e INFLUX_HOST            :: {influxdb_host}' +
     f'\n -e INFLUX_HOST_PORT       :: {influxdb_port}' +
     f'\n -e INFLUX_DATABASE        :: {influxdb_database}' +
-    f'\n -e INFLUX_RETENTION       :: {influxdb_retention}' +
-    f'\n -e INFLUX_SHARD           :: {influxdb_shard}' +
-    f'\n -e INFLUX_USER            :: {influxdb_user}' +
-    f'\n -e INFLUX_PASS            :: {influxdb_user_pass}' +
     f'\n -e GEO_MEASUREMENT        :: {geo_measurement}' +
     f'\n -e LOG_MEASUREMENT        :: {log_measurement}' +
     f'\n -e SEND_NGINX_LOGS        :: {send_nginx_logs}' +
@@ -296,8 +250,8 @@ def main():
         inode = stat(log_path).st_ino
         # Run main loop and grep a log file
         logparse(
-            log_path, influxdb_host, influxdb_port, influxdb_database, influxdb_user, influxdb_user_pass,
-            influxdb_retention, influxdb_shard, geo_measurement, log_measurement, send_nginx_logs, geoip_db_path, inode) # NOQA
+            log_path, influxdb_host, influxdb_port, influxdb_database, 
+            geo_measurement, log_measurement, send_nginx_logs, geoip_db_path, inode) # NOQA
 
 if __name__ == '__main__':
     try:
